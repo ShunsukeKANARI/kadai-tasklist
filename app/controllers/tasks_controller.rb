99d6049c2a241fad0caff91de21ce82a72612940
shunsuke.kanari <kanari01git@gmail.com>
2018-06-01 08:09:59 +0000

    edit tasks_controller & tasks/index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-   before_action :set_task, only: [:show, :edit, :update, :destroy]
-   before_action :require_user_logged_in
-   before_action :correct_user, only: [:new, :create,  :show, :edit, :destroy, :update]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     if logged_in?
@@ -55,12 +54,9 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
+      flash[:danger] = '不正なアクセスです'
       redirect_to root_url
     end
-  end
-  
-  def set_task
-    @task = Task.find(params[:id])
   end
   
   # Strong Parameter
